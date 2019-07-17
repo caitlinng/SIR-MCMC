1d85a188f075f928375fad29a514a28b0a1a6e61
caitlinng <51976249+caitlinng@users.noreply.github.com>
2019-07-17 11:07:09 +1000

    Add files via upload
    Chain working, though not converging
    Added R0 calculation. R0 shows some convergence, even though beta and gamma don't. Makes sense as R0 is a proportion of beta and gamma.

--- SIR_funcs.py
+++ SIR_funcs.py
@@ -15,6 +15,8 @@ def SIR_model(param):  # Where param = [beta, gamma]
     # Set parameters
     beta = param[0]
     gamma = param[1]
+    #beta = 0.20
+    #gamma = 0.14
 
     if beta/gamma <= 1:
         print ('Reproductive rate too low to sustain')
@@ -91,27 +93,21 @@ def SEIR_model(param):  # Where param = [beta, kappa, gamma]
 
 def SIR_ll(I_data, param): # Where I_data = I (infected individuals) as retrieved from data
     # Obtain model values for I, given new parameters
-    model_data = SIR_model(param)  # FIXME: Only taking in 1 parameter??
-    I_model = SIR_model(param).y[1] # FIXME: Only taking in 1 parameter??
-    print('For param = ' +str(param) +'I_model is: ' + str(I_model))
-    print('For param = ' +str(param) +'I_data is: ' + str(I_data))
+    model_data = SIR_model(param)
+    I_model = SIR_model(param).y[1]
+   # print('For param = ' + str(param) +'I_model is: ' + str(I_model))
+    #print('For param = ' + str(param) +'I_data is: ' + str(I_data))
     ll = 0
-    for k in range(len(I_data)): # FIXME: I_data and I_model now the same
-        if I_model[k] < 0.00001: #or int(I_model[k]) == 0:
-            I_model[k] = 0
-            #new_ll = 0
-            #print(str(k) + ' I_model was < 0')
-            #continue
 
-  #      else:
-        #    print('I_data[k] = ' + str(int(I_data[k])))
-        #    print('I_model[k] = ' + str(int(I_model[k])))
-
-
-        new_ll = st.poisson.logpmf(k = int(I_data[k]), mu = int(I_model[k]))
+    for k in range(len(I_data)):
+        new_ll = st.poisson.logpmf(k=int(I_data[k]), mu=int(I_model[k]))
+#        new_ll = st.norm.logpdf(I_data[k], loc=int(I_model[k]), scale=int(I_model[k]))
         #    print('new_ll = ' + str(new_ll))
 
         ll = ll + new_ll
+        return ll
+
+    '''
 
     plt.figure()
     plt.plot(model_data.t, I_model, label='model')
@@ -120,8 +116,8 @@ def SIR_ll(I_data, param): # Where I_data = I (infected individuals) as retrieve
     plt.ylabel('I')
     plt.show()
 
+'''
     #      if np.isnan(ll):  # can't move from -inf
 #            print(str(k) + ' was a nan ')
-    print(ll)
-    return ll
+
 
--- fit_SIR.py
+++ fit_SIR.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 # Produce synthetic data
 data_beta = 0.20
 data_gamma = 0.14
+data_R0 = data_beta / data_gamma
 data_param = [data_beta, data_gamma]
 
 data = SIR_funcs.SIR_model(data_param)
@@ -25,8 +26,9 @@ I_data = data.y[1]
 
 print('I_data' + str(I_data))
 plt.figure()
-plt.plot(data.t, data.y[0])
+plt.plot(data.t, data.y[1])
 
+'''
 plt.figure()
 plt.plot(data.t, I_data)
 
@@ -58,18 +60,22 @@ plt.plot(SEIR_data.t, SEIR_data.y[3])
 plt.plot(data.t, data.y[2])
 plt.ylabel('R')
 plt.show()
-
+'''
 
 '''
 MCMC Set-up
 '''
 # Starting guess [beta, gamma]
-
-#init_param = [st.norm.rvs(1, 1), st.norm.rvs(1, 1)]
-init_param = [0.20, 0.14]
+# TODO: Works if beta starting guess > gamma starting guess, but guesses never fulfill R0 >1 otherwise and programme breaks
+# TODO: If guesses too low (e.g. beta = 1, gamma = 0.5), then guesses keep going < 0 and programme breaks
+init_param = [st.norm.rvs(1, 1), st.norm.rvs(1, 1)]
+#init_param = [0.20, 0.14]
 
 # Proposal widths
 w = [0.05, 0.05]
+
+# Number of iterates
+n_iterates = 1000
 
 # Prior functions (what is our prior belief about beta, gamma)
 
@@ -79,28 +85,37 @@ def prior_param_belief(min, max):
 def prior_param_belief_normal(mean, var):
     return st.norm(loc=mean, scale=var)
 
+def R0_prior_fun(param):  # Produce R0 guess, where R0 > 1
+    R0_guess = 0
+    while R0_guess <1:
+        beta_guess = prior_param_belief_normal(1.0, 1.0)
+        gamma_guess = prior_param_belief_normal(1.0, 1.0)
+        R0_guess = beta_guess/gamma_guess
+
+    return [beta_guess, gamma_guess, R0_guess]
+
 # Prior belief is that beta and gamma are centred on 1 with sd of 1
-beta_prior_fun = prior_param_belief_normal(1.0, 0.9)
-gamma_prior_fun = prior_param_belief_normal(5.0, 3.0)
+beta_prior_fun = prior_param_belief_normal(1.0, 1.0)
+gamma_prior_fun = prior_param_belief_normal(1.0, 1.0)
 
 prior_funcs = [beta_prior_fun, gamma_prior_fun]
 
-# Number of iterates
-n_iterates = 100
 
 # Calculate the log likelihood of the initial guess
-init_ll = SIR_funcs.SIR_ll(I_data, init_param) # TODO: Should I be inputting init_param or data_param?
+init_ll = SIR_funcs.SIR_ll(I_data, init_param)
 
 # And log likelihood of all subsequent guesses
-def run_chain(data, n_iterates, w, init_param, init_ll, prior_funcs):
+def run_chain(I_data, n_iterates, w, init_param, init_ll, prior_funcs):
     param = init_param.copy()
     ll = init_ll.copy()
 
     # Establish data storage space for chain
     # Where first column is ll and 1: are the n-parameters [ll, beta, gamma]
-    chain = np.zeros((n_iterates, len(param) + 1))
+    chain = np.zeros((n_iterates, len(param) + 2))
     chain[0, 0] = ll
-    chain[0, 1:] = param
+    chain[0, 1] = param[0]
+    chain[0, 2] = param[1]
+    chain[0, 3] = param[0]/param[1]
 
     # Run MCMC
     for i in range(n_iterates):
@@ -116,22 +131,28 @@ def run_chain(data, n_iterates, w, init_param, init_ll, prior_funcs):
             prop_param = param.copy()
 
             # Take a random step size from a uniform distribution (that has width of w)
-            prop_param[j] = prop_param[j] - (sci.stats.uniform.rvs(loc=-w[j] / 2, scale=w[j], size=1))
+            R0_guess = 0
+            while R0_guess < 1:
+                prop_param[j] = prop_param[j] - (sci.stats.uniform.rvs(loc=-w[j] / 2, scale=w[j], size=1))
+                prop_param[j] = np.ndarray.item(prop_param[j]) # Converts paramater value from single element array into a scalar
+                R0_guess = prop_param[0]/prop_param[1]
 
             # Deal with invalid proposals by leaving ll, param unchanged
-            if prop_param[j] <= 0: # Invalid, so try next parameter proposal
-                prop_ll = -1 * np.inf
+            #if prop_param[j] <= 0 or prop_param[0]/prop_param[1] < 1:  # Invalid, so try next parameter proposal
+             #   prop_ll = -1 * np.inf
 
             else:
                 # Calculate LL of proposal
-
-                prop_ll = SIR_funcs.SIR_ll(data, prop_param)
+                prop_ll = SIR_funcs.SIR_ll(I_data, prop_param)
 
             # Decide on accept/reject
             prior_fun = prior_funcs[j]  # Grab correct prior function
 
             # Likelihood ratio st.norm.rvs(1, 1)
             r = np.exp(prop_ll - ll) * prior_fun.pdf(prop_param[j]) / prior_fun.pdf(param[j])
+            print('prop_ll = ' + str(prop_ll))
+            print('ll = ' + str(ll))
+            print('r = ' + str(r))
             # Is likelihood ratio less than or equal to one
             alpha = min(1, r)
 
@@ -146,22 +167,18 @@ def run_chain(data, n_iterates, w, init_param, init_ll, prior_funcs):
 
             # Store iterate
             chain[i, 0] = ll
-            chain[i, 1:] = param
+            chain[i, 1] = param[0]  # Store beta
+            chain[i, 2] = param[1]  # Store gamma
+            chain[i, 3] = param[0] / param[1]  # Store R0
 
     return chain
 
-print('np.shape(I_data) = ' + str(np.shape(I_data)))
-print('n_iterates = ' +str(np.shape(n_iterates)))
-print('w = ' + str(np.shape('w')))
-print('Init_param shape = ' + str(np.shape(init_param)))
-print('init_ll shape = ' + str(np.shape(init_ll)))
-print('prior_funcs shape = ' + str(np.shape(prior_funcs)))
 chain = run_chain(I_data, n_iterates, w, init_param, init_ll, prior_funcs)
 
 print (chain)
 
 # Show graphs
-chain = pd.DataFrame(chain, columns=['ll', 'beta', 'gamma']) # Change np to panda array
+chain = pd.DataFrame(chain, columns=['ll', 'beta', 'gamma', 'R0']) # Change np to panda array
 
 n = np.arange(int(n_iterates)) # Creates array of iterates
 
@@ -170,7 +187,7 @@ plt.ylabel('Log Likelihood')
 plt.xlabel('Iterate')
 
 chain.plot(kind = 'line', y = 'beta')
-#plt.plot(y = pbeta, color = 'r') # Plot true value as a single line
+plt.plot(y = data_beta, color = 'r') # Plot true value as a single line
 plt.ylabel('Estimate for beta')
 plt.xlabel('Iterate')
 
@@ -179,9 +196,15 @@ chain.plot(kind = 'line', y = 'gamma', color = 'b')
 plt.ylabel('Estimate for gamma')
 plt.xlabel('Iterate')
 
+chain.plot(kind = 'line', y = 'R0', color = 'b')
+#plt.plot(y = pgamma, color = 'r') # Plot true value as a single line
+plt.ylabel('Estimate for R0')
+plt.xlabel('Iterate')
+
 chain[['beta']].plot(kind = 'hist')
 
 chain[['gamma']].plot(kind = 'hist')
 
+chain[['R0']].plot(kind = 'hist')
 
 plt.show()
